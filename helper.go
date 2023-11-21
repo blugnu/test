0e39e03592c9f32b47a0c4f4a7bc8b3a6e7e13ec
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2023-11-21 19:57:59 +1300

    feat: Helper() test

---
+++ helper.go
@@ -0,0 +1,127 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type HelperResult bool
+
+const ShouldPass HelperResult = true
+const ShouldFail HelperResult = false
+
+// Helper runs a test helper function and compares the result to an
+// expected outcome.  The expected outcome (want) must be ONE of
+// the following:
+//
+//   - test.ShouldPass
+//   - test.ShouldFail
+//   - *test.ExpectedPanic (see: test.Panic())
+//
+// Example:
+//
+//	  func TestSomething(t *testing.T) {
+//		// ARRANGE
+//		var a, b int
+//
+//		// ACT
+//		a = 1
+//		b = 2
+//
+//		// ASSERT
+//		test.Helper(t, "test case 1", func(st *testing.T) {
+//		   test.Compare(st, 3, a+b)
+//		}, test.ShouldPass)
+//	  }
+func Helper(t *testing.T, f func(st *testing.T), want any) (CapturedOutput, CapturedOutput) {
+	t.Helper()
+
+	checkresult := true
+	result := true
+	err := error(nil)
+
+	switch expected := want.(type) {
+	case bool:
+		result = expected
+	case HelperResult:
+		result = bool(expected)
+	case *ExpectedPanic:
+		err = expected.error
+	case nil: // no outcome specified
+		checkresult = false
+	default:
+		panic(fmt.Errorf("%w: accepts only test.ShouldPass (or true), test.ShouldFail (or false) or test.ExpectPanic(error)", ErrInvalidArgument))
+	}
+
+	oc := map[bool]string{true: "PASS", false: "FAIL"}
+
+	stdout, stderr, _ := runInternal(t, func(st *testing.T) {
+		defer func() {
+			r := recover()
+			got, ok := r.(error)
+			switch {
+			case err == nil && r == nil:
+				return
+			case checkresult && err == nil && r != nil:
+				t.Errorf("\nwanted     : %s\ngot (panic): %v", oc[result], r)
+				return
+			case checkresult && err != nil && r == nil:
+				t.Errorf("\nwanted (panic): %v\ngot           : %s", err, oc[!st.Failed()])
+				return
+			case checkresult && !ok || !errors.Is(got, err):
+				t.Errorf("\nwanted (panic): %v\ngot    (panic): %v", err, r)
+			}
+		}()
+
+		st.Helper()
+		f(st)
+
+		if checkresult && result != !st.Failed() {
+			t.Errorf("\nwanted: %s\ngot   : %s", oc[result], oc[!st.Failed()])
+		}
+	})
+	return stdout, stderr
+}
+
+// matchAll is a test matcher function that always returns true and no error.
+// It is used by runInternal to ensure that the internal test is always
+// executed by the InternalTest runner, regardless of any match conditions
+// specified for the current *testing.T test run.
+func matchAll(pat, match string) (bool, error) {
+	return true, nil
+}
+
+// runInternal is a helper function that runs a specified test function as an
+// internal test.
+//
+// It is used by test.Helper() to run a test function in a separate test runner
+// in order to inspect the outcome of the test function without that affecting
+// the state of the current test.
+func runInternal(t *testing.T, f func(*testing.T)) (CapturedOutput, CapturedOutput, bool) {
+	result := false
+	stdout, stderr := CaptureOutput(t, func(t *testing.T) {
+		t.Helper()
+		sut := []testing.InternalTest{{
+			Name: "internal",
+			F:    f,
+		}}
+		result = testing.RunTests(matchAll, sut)
+	})
+
+	// this is a bit of a hack: when run with -v, the internal test run
+	// emits a RUN line and a PASS line when the test passes, which we don't
+	// want to see in the captured output.  So we remove them here.
+	//
+	// (if the internal test fails there is no additional output to worry about)
+
+	for i := len(stdout.s) - 1; i >= 0; i-- {
+		s := strings.TrimSpace(stdout.s[i])
+		if s == "=== RUN   internal" || strings.HasPrefix(s, "--- PASS: internal") {
+			stdout.s = append(stdout.s[:i], stdout.s[i+1:]...)
+		}
+	}
+
+	return stdout, stderr, result
+}
---
+++ helper_test.go
@@ -0,0 +1,99 @@
+package test
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestHelper(t *testing.T) {
+	t.Run("matchAll", func(t *testing.T) {
+		ok, err := matchAll("any", "none")
+		if !ok || err != nil {
+			t.Errorf("matchAll() = %v, %v, want %v, %v", ok, err, true, nil)
+		}
+	})
+
+	// ARRANGE
+	panerr := errors.New("panic err")
+
+	type args struct {
+		fn      func(*testing.T)
+		outcome any
+	}
+	testcases := []struct {
+		name    string
+		args                   // args passed to the Helper() function under test
+		outcome any            // expected outcome of the Helper() used to test Helper()
+		panic   *ExpectedPanic // expected panic, if any
+		output  any            // expected output
+	}{
+		{name: "invalid outcome", args: args{func(st *testing.T) {}, 42}, outcome: ExpectPanic(ErrInvalidArgument)},
+		{name: "fail, no outcome", args: args{fn: func(st *testing.T) { st.Fail() }}, outcome: ShouldPass},
+		{name: "pass, no outcome", args: args{fn: func(st *testing.T) {}}, outcome: ShouldPass},
+		{name: "panic, no outcome", args: args{fn: func(st *testing.T) { panic(42) }}, outcome: ShouldPass},
+		{name: "should fail", args: args{func(st *testing.T) { st.Fail() }, ShouldFail}, outcome: ShouldPass},
+		{name: "should fail (bool)", args: args{func(st *testing.T) { st.Fail() }, false}, outcome: ShouldPass},
+		{name: "should pass", args: args{func(st *testing.T) {}, ShouldPass}, outcome: ShouldPass},
+		{name: "should pass (bool)", args: args{func(st *testing.T) {}, true}, outcome: ShouldPass},
+		{name: "helper panics (expected err)", args: args{func(st *testing.T) { panic(panerr) }, ExpectPanic(panerr)}, outcome: ShouldPass},
+		{name: "helper panics (unexpected err)", args: args{func(st *testing.T) { panic(errors.New("other error")) }, ExpectPanic(panerr)},
+			outcome: ShouldFail,
+			output: []string{
+				"wanted (panic): panic err",
+				"got    (panic): other error",
+			},
+		},
+		{name: "should pass, panics", args: args{func(st *testing.T) { panic(panerr) }, ShouldPass},
+			outcome: ShouldFail,
+			output: []string{
+				"wanted     : PASS",
+				"got (panic): panic err",
+			},
+		},
+		{name: "should fail, panics", args: args{func(st *testing.T) { panic(panerr) }, ShouldFail},
+			outcome: ShouldFail,
+			output: []string{
+				"wanted     : FAIL",
+				"got (panic): panic err",
+			},
+		},
+		{name: "passed, should panic", args: args{func(st *testing.T) {}, ExpectPanic(panerr)},
+			outcome: ShouldFail,
+			output: []string{
+				"wanted (panic): panic err",
+				"got           : PASS",
+			},
+		},
+		{name: "failed, should panic", args: args{func(st *testing.T) { st.Fail() }, ExpectPanic(panerr)},
+			outcome: ShouldFail,
+			output: []string{
+				"wanted (panic): panic err",
+				"got           : FAIL",
+			},
+		},
+		{name: "verbose output from passing internal test is removed", args: args{func(st *testing.T) {
+			// _simulates_ the additional output from a test that passes
+			// when run with go test -v
+			os.Stdout.WriteString("=== RUN   internal\n")
+			os.Stdout.WriteString("--- PASS: internal (0.00s)\n")
+		}, ShouldPass}, outcome: ShouldPass, output: ""},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			// ARRANGE
+			defer tc.panic.Assert(t)
+
+			// HERE BE DRAGONS: the Helper test is used to test the Helper test
+			// (and capture the output from it)
+
+			// ACT
+			stdout, _ := Helper(t, func(st *testing.T) {
+				Helper(st, tc.args.fn, tc.args.outcome)
+			}, tc.outcome)
+
+			// ASSERT
+			stdout.Contains(t, tc.output)
+		})
+	}
+}
