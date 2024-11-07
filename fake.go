4ba3ca2d8a303e10d92e5d4b0fe1828a086c7d97
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2024-11-08 11:02:54 +1300

    feat(Fake[R]): new Returns() method for fluent configuration of result and error

--- fake.go
+++ fake.go
@@ -1,43 +1,96 @@
 package test
 
-// Fake is a generic type that can be used to fake a function call returning
-// some result type R and/or an error. It is useful for creating fakes for 
-// interface implmentations by composing an anonymous Fake[R] into a struct
-// implementing the desired method to be faked by returning the required values
-// from the Fake.
+import "fmt"
+
+// Fake[R] is a generic type that can be used to fake a function returning some
+// result type R and/or an error. It is useful for creating simple fakes for
+// functions or interface methods.
 //
-// By using an anonymous field of type Fake[R] in a struct, the Reset() method
-// of the Fake is promoted to the struct, allowing the struct to be reset to its
-// zero value. 
+// Fake[R] provides no mechanism for configuring expected calls, capturing or
+// testing for expected arguments, or returning different values for multiple
+// calls. It is intended for simple cases where a function or method is faked,
+// returning a fixed result value and/or error.
+//
+// For more complex cases, consider using test.MockFn[A, R] instead.
+//
+// # example
+//
+//	type MyInterface interface {
+//		MyMethod() (int, error)
+//	}
+//
+//	type mockMyMethodInterface struct {
+//		Fake[int]
+//	}
+//
+//	func (mock *MyFake) MyMethod() (int, error) {
+//		return mock.Result, mock.Err
+//	}
+//
+//	// there is no need to implement (mock *MyFake) Reset() as the Reset() method
+//	// of the embedded Fake[int] will be promoted to the mockMyMethodInterface struct.
+//
+// # Resetting a Fake
+//
+// A mock implementation should implement a Reset() method to reset any all
+// all Fake[R] (and any MockFn[A, R]) fields.
+//
+// When mocking an interface with a single method, an anonymous field may be
+// used in a struct implementing the interface, allowing the Reset() method of
+// the Fake[R] to be promoted to the struct itself.
+//
+// # Returning Multiple Values
 //
 // When faking an interface method that returns multiple result values (in
 // addition to an error), use a struct type with fields for each of the result
 // values.
 //
-// When faking an interface method that returns only result values and no error
-// simply ignore the Err field.  To fake a method that returns only an error,
-// it is recommended to specify a result type of any and ignore the Result field.
+// # Return Value(s) with No Error
 //
-// # Example
+// When faking a function that returns only result values and no error, simply ignore
+// the Err field.
 //
-// 	type MyInterface interface {
-// 		MyMethod() (int, error)
-// 	}
+// # Returning Only an Error
 //
-// 	type MyFake struct {
-// 		Fake[int]
-// 	}
-//
-// 	func (f *MyFake) MyMethod() (int, error) {
-// 		return f.Result, f.Err
-// 	}
+// To fake a method that returns only an error, specify a result type of any and
+// ignore the Result field.
 type Fake[R any] struct {
 	Result R
-	Err error
+	Err    error
 }
 
-// Reset sets the Result and Err fields to their zero values.
-func (f *Fake[R]) Reset() {
-	f.Result = *new(R)
-	f.Err = nil
+// Reset resets the Fake to its zero value.
+func (fake *Fake[R]) Reset() {
+	*fake = Fake[R]{}
+}
+
+// Returns sets the result value and/or error to be returned by the fake.
+//
+// The first R value in the variadic parameter list is used to set the result
+// value, and the first error value is used to set the error.
+//
+// If multiple R or error values are provided, the function will panic.  This
+// ensures that a test that is not setup correctly fails immediately without
+// yielding potentially incorrect results.
+func (fake *Fake[R]) Returns(v ...any) {
+	resultSet := false
+	errSet := false
+	for _, r := range v {
+		switch r := r.(type) {
+		case R:
+			if resultSet {
+				panic(fmt.Errorf("%w: multiple result values provided", ErrInvalidOperation))
+			}
+			resultSet = true
+			fake.Result = r
+		case error:
+			if errSet {
+				panic(fmt.Errorf("%w: multiple error values provided", ErrInvalidOperation))
+			}
+			errSet = true
+			fake.Err = r
+		default:
+			panic(fmt.Errorf("%w: %T: only values of type %T or error may be specified", ErrInvalidOperation, r, *new(R)))
+		}
+	}
 }
--- fake_test.go
+++ fake_test.go
@@ -1,6 +1,9 @@
 package test
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestFake(t *testing.T) {
 	// ARRANGE
@@ -11,4 +14,87 @@ func TestFake(t *testing.T) {
 
 	// ASSERT
 	That(t, fake.Result).Equals(0)
+}
+
+func TestFakeReturns(t *testing.T) {
+	// ARRANGE
+	testcases := []struct {
+		scenario string
+		exec     func(t *testing.T)
+	}{
+		{scenario: "returns value",
+			exec: func(t *testing.T) {
+				// ARRANGE
+				sut := Fake[int]{}
+
+				// ACT
+				sut.Returns(42)
+
+				// ASSERT
+				That(t, sut).Equals(Fake[int]{Result: 42})
+			},
+		},
+		{scenario: "returns error",
+			exec: func(t *testing.T) {
+				// ARRANGE
+				sut := Fake[int]{}
+				err := errors.New("fake error")
+
+				// ACT
+				sut.Returns(err)
+
+				// ASSERT
+				That(t, sut).Equals(Fake[int]{Err: err})
+			},
+		},
+		{scenario: "returns result and error",
+			exec: func(t *testing.T) {
+				// ARRANGE
+				sut := Fake[int]{}
+				err := errors.New("fake error")
+
+				// ACT
+				sut.Returns(42, err)
+
+				// ASSERT
+				That(t, sut).Equals(Fake[int]{Result: 42, Err: err})
+			},
+		},
+		{scenario: "multiple result values",
+			exec: func(t *testing.T) {
+				// ARRANGE + ASSERT
+				sut := Fake[int]{}
+				defer ExpectPanic(ErrInvalidOperation).Assert(t)
+
+				// ACT
+				sut.Returns(1, 2)
+			},
+		},
+		{scenario: "multiple error values",
+			exec: func(t *testing.T) {
+				// ARRANGE + ASSERT
+				sut := Fake[int]{}
+				err := errors.New("fake error")
+				defer ExpectPanic(ErrInvalidOperation).Assert(t)
+
+				// ACT
+				sut.Returns(err, err)
+			},
+		},
+		{scenario: "invalid type",
+			exec: func(t *testing.T) {
+				// ARRANGE + ASSERT
+				sut := Fake[int]{}
+				defer ExpectPanic(ErrInvalidOperation).Assert(t)
+
+				// ACT
+				sut.Returns("invalid type")
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.scenario, func(t *testing.T) {
+			tc.exec(t)
+		})
+	}
 }
