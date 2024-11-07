7f90f805c17be6f091e654209a6fa841060a6dc3
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2024-11-08 10:59:42 +1300

    feat: mock configuration and expectation errors

--- errors.go
+++ errors.go
@@ -5,7 +5,15 @@ import "errors"
 var (
 	ErrCapture           = errors.New("test.Capture")
 	ErrInvalidArgument   = errors.New("invalid argument")
+	ErrInvalidOperation  = errors.New("invalid operation")
 	ErrInvalidOptionType = errors.New("invalid option type")
 	ErrInvalidTest       = errors.New("invalid test")
 	ErrNotNilable        = errors.New("values of this type are not nilable")
+
+	ErrExpectationsNotMet = errors.New("expectations not met")
+	ErrExpectedArgs       = errors.New("arguments were expected but not recorded")
+	ErrNoResultForArgs    = errors.New("no result for arguments")
+	ErrUnexpectedArgs     = errors.New("the arguments recorded did not match those expected")
+	ErrUnexpectedCall     = errors.New("unexpected call")
+	ErrResultNotUsed      = errors.New("result not used")
 )
